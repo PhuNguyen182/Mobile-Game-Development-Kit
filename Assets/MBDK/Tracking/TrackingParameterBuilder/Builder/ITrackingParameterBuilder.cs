773b595e3e34ade3fa3ef030539218ef4f8e226c
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2025-10-19 21:41:38 +0700

    Update builder code

--- Assets/MBDK/Tracking/TrackingParameterBuilder/Builder/ITrackingParameterBuilder.cs
+++ Assets/MBDK/Tracking/TrackingParameterBuilder/Builder/ITrackingParameterBuilder.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MBDK.Tracking.TrackingParameterBuilder.CustomParameters;
 using MBDK.Tracking.TrackingParameterBuilder.Interfaces;
 
 namespace MBDK.Tracking.TrackingParameterBuilder.Builder
@@ -9,7 +10,16 @@ namespace MBDK.Tracking.TrackingParameterBuilder.Builder
         
         public List<ITrackerParameter> GetParameters();
         public void AddParameter(ITrackerParameter parameter);
-        public void RemoveParameter(ITrackerParameter parameter);
+        public void AddParameter(IntTrackingParameter parameter);
+        public void AddParameter(LongTrackingParameter parameter);
+        public void AddParameter(FloatTrackingParameter parameter);
+        public void AddParameter(DoubleTrackingParameter parameter);
+        public void AddParameter(StringTrackingParameter parameter);
+        public void AddParameter(string parameterName, int parameterValue);
+        public void AddParameter(string parameterName, long parameterValue);
+        public void AddParameter(string parameterName, float parameterValue);
+        public void AddParameter(string parameterName, double parameterValue);
+        public void AddParameter(string parameterName, string parameterValue);
         public void ClearParameters();
     }
 }
--- Assets/MBDK/Tracking/TrackingParameterBuilder/Builder/TrackingParameterBuilder.cs
+++ Assets/MBDK/Tracking/TrackingParameterBuilder/Builder/TrackingParameterBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MBDK.Tracking.TrackingParameterBuilder.CustomParameters;
 using MBDK.Tracking.TrackingParameterBuilder.Interfaces;
 
 namespace MBDK.Tracking.TrackingParameterBuilder.Builder
@@ -16,9 +17,59 @@ namespace MBDK.Tracking.TrackingParameterBuilder.Builder
             this._parameters.Add(parameter);
         }
 
-        public void RemoveParameter(ITrackerParameter parameter)
+        public void AddParameter(IntTrackingParameter parameter)
         {
-            this._parameters.Remove(parameter);
+            this._parameters.Add(parameter);
+        }
+
+        public void AddParameter(LongTrackingParameter parameter)
+        {
+            this._parameters.Add(parameter);
+        }
+
+        public void AddParameter(FloatTrackingParameter parameter)
+        {
+            this._parameters.Add(parameter);
+        }
+
+        public void AddParameter(DoubleTrackingParameter parameter)
+        {
+            this._parameters.Add(parameter);
+        }
+
+        public void AddParameter(StringTrackingParameter parameter)
+        {
+            this._parameters.Add(parameter);
+        }
+
+        public void AddParameter(string parameterName, int parameterValue)
+        {
+            IntTrackingParameter parameter = new IntTrackingParameter(parameterName, parameterValue);
+            this.AddParameter(parameter);
+        }
+
+        public void AddParameter(string parameterName, long parameterValue)
+        {
+            LongTrackingParameter parameter = new LongTrackingParameter(parameterName, parameterValue);
+            this.AddParameter(parameter);
+        }
+
+        public void AddParameter(string parameterName, float parameterValue)
+        {
+            FloatTrackingParameter parameter = new FloatTrackingParameter(parameterName, parameterValue);
+            this.AddParameter(parameter);
+        }
+
+        public void AddParameter(string parameterName, double parameterValue)
+        {
+            DoubleTrackingParameter parameter = new DoubleTrackingParameter(parameterName, parameterValue);
+            this.AddParameter(parameter);
+        }
+
+        public void AddParameter(string parameterName, string parameterValue)
+        {
+            StringTrackingParameter parameter = new StringTrackingParameter(parameterName, parameterValue);
+            this.AddParameter(parameter);
         }
 
         public void ClearParameters()
